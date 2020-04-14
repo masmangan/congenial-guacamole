9de82403f1a347622b159b680832e6acf23f2fe6
Marco Mangan <marco.mangan@gmail.com>
2020-04-13 21:32:32 -0300

    Versão inicial.

---
+++ t1/src/test/java/pucrs/si/qp/t1/AppTest.java
@@ -0,0 +1,18 @@
+package pucrs.si.qp.t1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+
+{
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
